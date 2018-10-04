8e8d61b6d35afddb7c6c9c937197844e419d3074
Niki Ahlskog <niki.ahlskog@gmail.com>
2018-10-05 01:20:43 +0300

    filters work but eventlisteners not

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-const contentDiv = document.getElementById('content');
+const contentDiv = document.getElementById('content')
 const chunks = _.chunk(audioList, 4)
+const categories = _.uniq(audioList.map(item => item.show))
+const categoriesButtonsDiv = document.querySelector('.btn-group')
 
-chunks.map(chunk =>
+const renderCategoriesButtons = () => {
+  categoriesButtonsDiv.innerHTML += `
+  ${categories.map(category => `<button style="width:${100 / categories.length}%">${category}</button>`).join('')}
+  <button style="width:${100 / categories.length}%">Kaikki</button>
+  `
+}
+renderCategoriesButtons()
+
+const renderItems = (renders) => {
+  contentDiv.innerHTML = ''
+  return renders.map(chunk =>
   contentDiv.innerHTML += `
     <div class="row">
     ${chunk.map(audio => `
@@ -12,12 +24,28 @@ chunks.map(chunk =>
       <audio data-name="${audio.audio}" src="sounds/${audio.audio}.mp3"></audio>
       `).join('')}
     </div>`).join('')
+}
+renderItems(chunks)
+
+const categoryButtons = Array.from(document.querySelectorAll('.btn-group')[0].getElementsByTagName('button'))
+
+const filterShowsAndRender = (showName) => {
+  if (showName === 'Kaikki') {
+    renderItems(_.chunk(audioList, 4))
+    return;
+  }
+  const filteredShow = audioList.filter(audio => audio.show === showName)
+  renderItems(_.chunk(filteredShow, 4))
+}
+
+categoryButtons.forEach(categoryButton => categoryButton.addEventListener('click', () => filterShowsAndRender(categoryButton.innerText)))
+
 
 const columns = Array.from(document.querySelectorAll('.column'))
 const audios = Array.from(document.getElementsByTagName('audio'))
 const images = Array.from(document.querySelectorAll('img'))
 
-function checkOtherImagesDoesntHavePlayingClass({ dataset }) {
+const checkOtherImagesDoesntHavePlayingClass = ({ dataset }) => {
   images.forEach(img => {
     if (
       img.classList.contains('playing') &&
@@ -28,7 +56,7 @@ function checkOtherImagesDoesntHavePlayingClass({ dataset }) {
   })
 }
 
-function playSound({ currentTarget }) {
+const playSound = ({ currentTarget }) => {
   const wot = currentTarget.dataset.audio
   const audio = document.querySelector(`audio[data-name="${wot}"]`)
   const image = document.querySelector(`img[data-name="${wot}"]`)
@@ -42,16 +70,17 @@ function playSound({ currentTarget }) {
   checkOtherImagesDoesntHavePlayingClass(currentTarget)
 }
 
+
 document.addEventListener(
-  'play',
-  ({ target }) => {
-    audios.forEach(item => {
-      if (item !== target) {
-        item.pause()
-      }
-    })
-  },
-  true
+'play',
+({ target }) => {
+  audios.forEach(item => {
+    if (item !== target) {
+      item.pause()
+    }
+  })
+},
+true
 )
 
 columns.forEach(column => column.addEventListener('click', playSound))
--- sounds.js
+++ sounds.js
@@ -1,61 +1,73 @@
 const audioList = [{
   "name": "No vittu mieti",
   "audio": "novittumieti",
+  "show": "Pasila",
   "tags": ["pasila", "no vittu mieti"]
 },
 {
   "name": "Mä annoin sulle tonnin setelin",
   "audio": "tonninseteli",
+  "show": "Kummeli",
   "tags": ["kummeli", "tonninseteli"]
 },
 {
   "name": "Ja tuhanteen",
   "audio": "tuhanteen",
+  "show": "Kummeli",
   "tags": ["kummeli", "tuhanteen"]
 },
 {
   "name": "Me on hävitty tää peli",
   "audio": "havittypeli",
+  "show": "Kummeli",
   "tags": ["kummeli", "hävitty peli", "havittypeli"]
 },
 {
   "name": "Mitä vittua täällä tapahtuu",
   "audio": "mitavittua",
+  "show": "Pasila",
   "tags": ["pasila", "mitavittua", "mitä vittua täällä tapahtuu"]
 },
 {
   "name": "Millanen päivä sulla on huomenna",
   "audio": "millanenpaiva",
+  "show": "Kummeli",
   "tags": ["kummeli", "millanenpaiva"]
 },
 {
   "name": "Älä leiki jeesusta",
   "audio": "jeesus",
+  "show": "Risumies",
   "tags": ["risumies", "jeesus"]
 },
 {
   "name": "Rypäleitä perseessä",
   "audio": "rypaleita",
+  "show": "Kummeli",
   "tags": ["kummeli", "rypaleita"]
 },
 {
   "name": "Vittu mikä kuumotus",
   "audio": "kuumotus",
+  "show": "Madventures",
   "tags": ["Madventures", "kuumotus"]
 },
 {
   "name": "Mee vittu töihin",
   "audio": "toihin",
+  "show": "Random",
   "tags": ["toihin", "toihin"]
 },
 {
   "name": "Fiilunkipeliä",
   "audio": "fiilunkia",
+  "show": "Kummeli",
   "tags": ["kummeli", "fiilunkia"]
 },
 {
   "name": "Haista sinä mursu paska",
   "audio": "mursu",
+  "show": "Kummeli",
   "tags": ["kummeli", "mursu"]
 }
 ];
