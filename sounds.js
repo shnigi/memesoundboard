29adaa8c759e2614078593a657a3b011c30729cc
Niki Ahlskog <niki.ahlskog@gmail.com>
2018-10-04 20:33:15 +0300

    sounds list

---
+++ sounds.js
@@ -0,0 +1,61 @@
+const audioList = [{
+  "name": "No vittu mieti",
+  "audio": "novittumieti",
+  "tags": ["pasila", "no vittu mieti"]
+},
+{
+  "name": "Mä annoin sulle tonnin setelin",
+  "audio": "tonninseteli",
+  "tags": ["kummeli", "tonninseteli"]
+},
+{
+  "name": "Ja tuhanteen",
+  "audio": "tuhanteen",
+  "tags": ["kummeli", "tuhanteen"]
+},
+{
+  "name": "Me on hävitty tää peli",
+  "audio": "havittypeli",
+  "tags": ["kummeli", "hävitty peli", "havittypeli"]
+},
+{
+  "name": "Mitä vittua täällä tapahtuu",
+  "audio": "mitavittua",
+  "tags": ["pasila", "mitavittua", "mitä vittua täällä tapahtuu"]
+},
+{
+  "name": "Millanen päivä sulla on huomenna",
+  "audio": "millanenpaiva",
+  "tags": ["kummeli", "millanenpaiva"]
+},
+{
+  "name": "Älä leiki jeesusta",
+  "audio": "jeesus",
+  "tags": ["risumies", "jeesus"]
+},
+{
+  "name": "Rypäleitä perseessä",
+  "audio": "rypaleita",
+  "tags": ["kummeli", "rypaleita"]
+},
+{
+  "name": "Vittu mikä kuumotus",
+  "audio": "kuumotus",
+  "tags": ["Madventures", "kuumotus"]
+},
+{
+  "name": "Mee vittu töihin",
+  "audio": "toihin",
+  "tags": ["toihin", "toihin"]
+},
+{
+  "name": "Fiilunkipeliä",
+  "audio": "fiilunkia",
+  "tags": ["kummeli", "fiilunkia"]
+},
+{
+  "name": "Haista sinä mursu paska",
+  "audio": "mursu",
+  "tags": ["kummeli", "mursu"]
+}
+];
